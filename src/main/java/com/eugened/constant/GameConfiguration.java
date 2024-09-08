a28f77380c254103a64a9ec8cf5bf4373b3c7e3f
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2024-09-09 00:23:16 +0300

    General changeFile with changing parameterss

---
+++ src/main/java/com/eugened/constant/GameConfiguration.java
@@ -0,0 +1,12 @@
+package com.eugened.constant;
+
+public class GameConfiguration {
+    public static final int HEIGHT = 10;
+    public static final int WIDTH = 10;
+
+    public static final int MIN_ANIMALS_GENARATE = 5;
+    public static final int MAX_ANIMALS_GENARATE = 10;
+
+    public static final int TIME_TICK = 10;
+    public static final int COUNT_TICK = 10;
+}
