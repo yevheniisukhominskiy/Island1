9bcfeb809531383a4cbf47a51d5c3ca007878403
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2024-09-09 00:16:59 +0300

    Animal added

---
+++ src/main/java/com/eugened/entity/object/organism/animal/herbivore/creatures/Caterpillar.java
@@ -0,0 +1,12 @@
+package com.eugened.entity.object.organism.animal.herbivore.creatures;
+
+import com.eugened.annotation.GameConfig;
+import com.eugened.annotation.GameMember;
+import com.eugened.entity.object.organism.animal.herbivore.Herbivore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+@GameMember
+@GameConfig(fileName = "config/entities/organisms/animals/herbivore/caterpillar.yaml")
+public class Caterpillar extends Herbivore {
+}
