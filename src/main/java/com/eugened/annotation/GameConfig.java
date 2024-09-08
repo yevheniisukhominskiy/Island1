8b392b94748189c2d11e2298adf812357397c417
Yevhenii Sukhominskiy <114820378+yevheniisukhominskiy@users.noreply.github.com>
2024-09-09 00:22:25 +0300

    General changes

--- src/main/java/com/eugened/annotation/GameConfig.java
+++ src/main/java/com/eugened/annotation/GameConfig.java
@@ -1,5 +1,9 @@
 package com.eugened.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface GameConfig {
-    // TODO: Make an annotation implementation @interface GameConfig
+    String fileName();
 }
